41ecc02b0b674217181119a18e42dbf06b419d10
Ed Finn <ed@iowacollegefoundation.org>
2021-09-25 09:06:44 -0500

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,92 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+        public DbSet<College> Colleges { get; set; }
+        public DbSet<EmpIndustry> EmpIndustries { get; set; }
+        public DbSet<EmpInfo> EmpInfos { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Major> Majors { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<StudInfo> StudInfos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // modelBuilder.Entity<AppUser>()
+            //     .HasMany(ur => ur.UserRoles)
+            //     .WithOne(u => u.User)
+            //     .HasForeignKey(ur => ur.UserId)
+            //     .IsRequired();
+
+            // modelBuilder.Entity<AppRole>()
+            //   .HasMany(ur => ur.UserRoles)
+            //   .WithOne(u => u.Role)
+            //   .HasForeignKey(ur => ur.RoleId)
+            //   .IsRequired();
+
+            // modelBuilder.Entity<CollegeMajor>()
+            //     .HasKey(cm => new { cm.CollegeNum, cm.MajorId });
+
+            // modelBuilder.Entity<CollegeMajor>()
+            //     .HasOne(cm => cm.ColUser)
+            //     .WithMany(c => c.CollegeMajors)
+            //     .HasForeignKey(cm => cm.CollegeNum)
+            //     .OnDelete(DeleteBehavior.Cascade);
+
+            // modelBuilder.Entity<CollegeMajor>()
+            //     .HasOne(cm => cm.Major)
+            //     .WithMany(m => m.CollegeMajors)
+            //     .HasForeignKey(cm => cm.MajorId)
+            //     .OnDelete(DeleteBehavior.Cascade);
+
+            // modelBuilder.Entity<Major>()
+            //     .HasOne(m => m.MajorCat)
+            //     .WithMany(mc => mc.Majors)
+            //     .HasForeignKey(m => m.MajorCatId)
+            //     .OnDelete(DeleteBehavior.Cascade);
+
+            // modelBuilder.Entity<FactFeature>()
+            //     .HasOne(ff => ff.ColUser)
+            //     .WithMany(c => c.FactFeatures)
+            //     .HasForeignKey(ff => ff.CollegeNum)
+            //     .OnDelete(DeleteBehavior.Cascade);
+
+            // modelBuilder.Entity<ColUserLike>()
+            //     .HasKey(k => new { k.SourceColUserId, k.LikedColUserId });
+
+            // modelBuilder.Entity<ColUserLike>()
+            //     .HasOne(s => s.SourceColUser)
+            //     .WithMany(l => l.LikedColUsers)
+            //     .HasForeignKey(s => s.SourceColUserId)
+            //     .OnDelete(DeleteBehavior.NoAction);
+
+            // modelBuilder.Entity<ColUserLike>()
+            //     .HasOne(s => s.LikedColUser)
+            //     .WithMany(l => l.LikedByColUsers)
+            //     .HasForeignKey(s => s.LikedColUserId)
+            //     .OnDelete(DeleteBehavior.NoAction);
+
+            // modelBuilder.Entity<Message>()
+            //     .HasOne(u => u.Recipient)
+            //     .WithMany(m => m.MessagesReceived)
+            //     .OnDelete(DeleteBehavior.Restrict);
+
+            // modelBuilder.Entity<Message>()
+            //     .HasOne(u => u.Sender)
+            //     .WithMany(m => m.MessagesSent)
+            //     .OnDelete(DeleteBehavior.Restrict);
+
+            // modelBuilder.ApplyUtcDateTimeConverter();
+        }
+    }
+}
---
+++ API/Data/Migrations/20210925132403_InitialCreate.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Colleges",
+                columns: table => new
+                {
+                    CollegeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CollegeName = table.Column<string>(type: "nvarchar(60)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colleges", x => x.CollegeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmpIndustries",
+                columns: table => new
+                {
+                    EmpIndustryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpIndustryName = table.Column<string>(type: "nvarchar(60)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmpIndustries", x => x.EmpIndustryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    LocationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LocationName = table.Column<string>(type: "nvarchar(60)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.LocationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Majors",
+                columns: table => new
+                {
+                    MajorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MajorName = table.Column<string>(type: "nvarchar(60)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Majors", x => x.MajorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    AppUserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(255)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(30)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(30)", nullable: true),
+                    AppUserType = table.Column<string>(type: "varchar(12)", nullable: true),
+                    Active = table.Column<bool>(type: "bit", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(60)", nullable: true),
+                    ClassYear = table.Column<string>(type: "varchar(12)", nullable: true),
+                    Major = table.Column<string>(type: "nvarchar(60)", nullable: true),
+                    College = table.Column<string>(type: "varchar(30)", nullable: true),
+                    EmpName = table.Column<string>(type: "nvarchar(60)", nullable: true),
+                    EmpIndustry = table.Column<string>(type: "varchar(30)", nullable: true),
+                    EmployeeNum = table.Column<string>(type: "varchar(30)", nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastActive = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.AppUserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmpInfos",
+                columns: table => new
+                {
+                    EmpInfoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpWebsite = table.Column<string>(type: "nvarchar(255)", nullable: true),
+                    CompanyDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WhyWork = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AppUserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmpInfos", x => x.EmpInfoId);
+                    table.ForeignKey(
+                        name: "FK_EmpInfos_Users_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "Users",
+                        principalColumn: "AppUserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LogoUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HrUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateAdded = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsMain = table.Column<bool>(type: "bit", nullable: false),
+                    IsMainLogo = table.Column<bool>(type: "bit", nullable: false),
+                    IsMainHr = table.Column<bool>(type: "bit", nullable: false),
+                    PublicId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AppUserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Photos_Users_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "Users",
+                        principalColumn: "AppUserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Positions",
+                columns: table => new
+                {
+                    PositionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PositionName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    PositionDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LookingFor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PositionType = table.Column<string>(type: "varchar(25)", nullable: true),
+                    PositionLocation = table.Column<string>(type: "nvarchar(60)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AppDeadline = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false),
+                    HrContact = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    HrContactTitle = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    HowToApply = table.Column<string>(type: "nvarchar(256)", nullable: true),
+                    ApplyEmail = table.Column<string>(type: "nvarchar(255)", nullable: true),
+                    AppUserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Positions", x => x.PositionId);
+                    table.ForeignKey(
+                        name: "FK_Positions_Users_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "Users",
+                        principalColumn: "AppUserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudInfos",
+                columns: table => new
+                {
+                    StudInfoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GPA = table.Column<string>(type: "varchar(10)", nullable: true),
+                    GradDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BestEmail = table.Column<string>(type: "nvarchar(255)", nullable: true),
+                    BestPhone = table.Column<string>(type: "varchar(30)", nullable: true),
+                    Athletics = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Arts = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExtraCurricular = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AcademicPlus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WorkPlus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DreamJob = table.Column<string>(type: "nvarchar(256)", nullable: true),
+                    AppUserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudInfos", x => x.StudInfoId);
+                    table.ForeignKey(
+                        name: "FK_StudInfos_Users_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "Users",
+                        principalColumn: "AppUserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmpInfos_AppUserId",
+                table: "EmpInfos",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_AppUserId",
+                table: "Photos",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Positions_AppUserId",
+                table: "Positions",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudInfos_AppUserId",
+                table: "StudInfos",
+                column: "AppUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Colleges");
+
+            migrationBuilder.DropTable(
+                name: "EmpIndustries");
+
+            migrationBuilder.DropTable(
+                name: "EmpInfos");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Majors");
+
+            migrationBuilder.DropTable(
+                name: "Photos");
+
+            migrationBuilder.DropTable(
+                name: "Positions");
+
+            migrationBuilder.DropTable(
+                name: "StudInfos");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
