2b13ac8c3007666acf3c67c4eb33357671a3c9b6
Ed Finn <ed@iowacollegefoundation.org>
2022-02-05 10:27:57 -0600

    part way thru photo 2-5-22

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
 import { take } from 'rxjs/operators';
 import { UserLoginComponent } from 'src/app/user-login/user-login.component';
 import { Member } from 'src/app/_models/member';
 import { User } from 'src/app/_models/user';
 import { AccountService } from 'src/app/_services/account.service';
 import { MembersService } from 'src/app/_services/members.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-photo-editor',
@@ -14,6 +16,9 @@ import { MembersService } from 'src/app/_services/members.service';
 export class PhotoEditorComponent implements OnInit {
   @Input() member: Member;
   user: User;
+  uploader: FileUploader;
+  hasBaseDropzoneOver = false;
+  baseUrl = environment.apiUrl;
 
   constructor(
     private accountService: AccountService,
@@ -24,5 +29,34 @@ export class PhotoEditorComponent implements OnInit {
       .subscribe((user) => (this.user = user));
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initializeUploader();
+  }
+
+  fileOverBase(event: any) {
+    this.hasBaseDropzoneOver = event;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'searchusers/add-photo',
+      authToken: 'Bearer ' + this.user.token,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024,
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const photo = JSON.parse(response);
+        this.member.photos.push(photo);
+      }
+    };
+  }
 }
