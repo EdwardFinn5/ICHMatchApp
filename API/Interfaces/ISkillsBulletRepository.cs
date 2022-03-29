63d772ec7da1637d0a5f336d7a4af28cb0265629
Ed Finn <ed@iowacollegefoundation.org>
2022-03-28 19:21:54 -0500

    getting into skills bullet

--- API/Interfaces/ISkillsBulletRepository.cs
+++ API/Interfaces/ISkillsBulletRepository.cs
@@ -7,6 +7,13 @@ namespace API.Interfaces
 {
     public interface ISkillsBulletRepository
     {
-        
+        void Update(DutyBullet dutyBullet);
+        Task<bool> SaveAllAsync();
+        // Task<IEnumerable<DutyBulletDto>> GetDutyBulletDtosByAppUserIdAsync(int id);
+        Task<IEnumerable<DutyBulletDto>> GetDutyBulletDtosByPositionIdAsync(int id);
+        Task<DutyBulletDto> GetDutyBulletDtoByIdAsync(int id);
+        Task<DutyBullet> GetDutyBulletByIdAsync(int id);
+        void DeleteDutyBullet(DutyBullet dutyBullet);
+        Task<bool> Complete();
     }
 }
